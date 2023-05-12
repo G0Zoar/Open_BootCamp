04a87b41b52196cd9caccb7892351f2c40fc760e
Fernando Ezequiel Troncoso <131387303+G0Zoar@users.noreply.github.com>
2023-05-11 21:20:36 -0300

    Add files via upload

---
+++ Introduccion_a_la_Programacion/src/com/OpenBootCamp/Ejercicios_TEMA_3.java
@@ -0,0 +1,38 @@
+package com.OpenBootCamp;
+
+public class Ejercicios_TEMA_3 {
+    public static void main(String[] args) {
+        System.out.println("Introducción a la Programación!");
+
+        //Ejercicios TEMA 3 'Primera Parte'
+
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        var resultado = suma(a, b, c);
+        System.out.println(resultado);
+
+        //Ejercicios TEMA 3 'Segunda Parte'
+
+        Coche miCoche = new Coche();
+        miCoche.SumarPuertas();
+        miCoche.SumarPuertas();
+        miCoche.SumarPuertas();
+        System.out.println(miCoche.puertas);
+
+    }
+
+    //Ejercicios TEMA 3 'Primera Parte'
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    //Ejercicios TEMA 3 'Segunda Parte'
+    static class Coche {
+        public int puertas = 2;
+        public void SumarPuertas() {
+            this.puertas++;
+        }
+    }
+}
