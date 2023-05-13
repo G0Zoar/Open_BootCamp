ccfabf7ed1c642201fe1945dc70858d173f676e0
Fernando Ezequiel Troncoso <131387303+G0Zoar@users.noreply.github.com>
2023-05-13 12:35:18 -0300

    Add files via upload

---
+++ Java_Basico/src/com/Conceptos/TipoDeDatos.java
@@ -0,0 +1,14 @@
+package com.Conceptos;
+
+public class TipoDeDatos {
+    public static void main(String[] args) {
+        System.out.println("byte, short, int, long");
+        System.out.println("float, double");
+        System.out.println("char");
+        System.out.println("boolean");
+        System.out.println("String");
+        System.out.println("Integer");
+        System.out.println("Long");
+    }
+}
+// ALUMNO: Ezequiel Troncoso .
