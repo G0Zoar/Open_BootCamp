8a2025fe76b4ce38d615ab4f9ec07b5cf10daaa9
G0Zoar <gaming.with.zoar@gmail.com>
2023-05-15 15:55:55 -0300

    Update !

---
+++ Java_Basico/src/com/Conceptos/BuclesFor.java
@@ -0,0 +1,12 @@
+package com.Conceptos;
+
+public class BuclesFor {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 3; i++) {
+            System.out.println(i + " Hola " + "Fer");
+            if (i == 3) {
+                System.out.println("Verify");
+            }
+        }
+    }
+}
