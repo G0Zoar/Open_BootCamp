ff3db6c1caaca9b98c03687139949b3148f41c80
G0Zoar <gaming.with.zoar@gmail.com>
2023-05-16 00:55:34 -0300

    Update !

---
+++ Java_Basico/src/com/EjerciciosResueltos/TipoDeDatos.java
@@ -0,0 +1,22 @@
+package com.EjerciciosResueltos;
+
+public class TipoDeDatos {
+    public static void main(String[] args) {
+
+        System.out.println("Tipos de DATOS!");
+
+        System.out.println("byte");
+        System.out.println("short");
+        System.out.println("int");
+        System.out.println("long");
+        System.out.println("float");
+        System.out.println("double");
+        System.out.println("char");
+        System.out.println("boolean");
+        System.out.println("Integer");
+        System.out.println("Long");
+        System.out.println("String");
+
+    }
+}
+// ALUMNO: Ezequiel Troncoso !
