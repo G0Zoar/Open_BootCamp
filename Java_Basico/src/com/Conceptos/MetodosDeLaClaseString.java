5538ae413dfa71645f38504594bd4b1f1fd3822c
G0Zoar <gaming.with.zoar@gmail.com>
2023-05-15 21:30:41 -0300

    Update !

---
+++ Java_Basico/src/com/Conceptos/MetodosDeLaClaseString.java
@@ -0,0 +1,25 @@
+package com.Conceptos;
+
+public class MetodosDeLaClaseString {
+    /*
+    los metodos de la clase string
+      es un tipo de datos que nos permite trabajar con cadena de textos
+     */
+
+    // La clase String
+
+    /*
+
+    length()
+    startWith(**)
+    endsWith(**)
+    indexOf(**)
+    subString(1.5)
+    trim()
+    equals()
+    compareTo
+
+     */
+    String mensaje = "Hola Mundo";
+
+}
---
+++ Java_Basico/src/com/Conceptos/TecnicasDeRefactoring.java
@@ -0,0 +1,8 @@
+package com.Conceptos;
+
+public class TecnicasDeRefactoring {
+    /*
+    consiste en adaptar un codigo para hacerlo mucho más legible y
+    que se pueda mantener mejor a corto y largo plazo
+     */
+}
