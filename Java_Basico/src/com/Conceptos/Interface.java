bbb13c0e5be80e7f206922cfeedf9e8e214393ae
G0Zoar <gaming.with.zoar@gmail.com>
2023-05-15 21:07:56 -0300

    Update !

--- Java_Basico/src/com/Conceptos/Interface.java
+++ Java_Basico/src/com/Conceptos/Interface.java
@@ -1,4 +1,8 @@
 package com.Conceptos;
 
 public class Interface {
+    /*
+    un mecanismo de abstraccion que nos va a permitir desacoplar el codigo,
+    y hacerlo mucho más flexible a la vez que se puede mantener mucho mejor.
+     */
 }
