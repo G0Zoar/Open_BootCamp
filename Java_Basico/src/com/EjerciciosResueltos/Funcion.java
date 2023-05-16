4c731d198ad7d62b9f6c2426a4e28817dc1deb68
G0Zoar <gaming.with.zoar@gmail.com>
2023-05-16 00:50:19 -0300

    Update !

---
+++ Java_Basico/src/com/EjerciciosResueltos/Funcion.java
@@ -0,0 +1,14 @@
+package com.EjerciciosResueltos;
+
+public class Funcion {
+    public static void main(String[] args) {
+        precioMasIva();
+    }
+    static void precioMasIva() {
+        int precio = 100;
+        int iva = 21;
+        int resultado = precio + iva;
+        System.out.println(resultado);
+
+    }
+}
