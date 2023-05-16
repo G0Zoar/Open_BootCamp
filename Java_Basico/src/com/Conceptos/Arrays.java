e126f11782cbda5c8bdbe2f194ed89091d5c5f08
G0Zoar <gaming.with.zoar@gmail.com>
2023-05-15 21:44:43 -0300

    Update !

---
+++ Java_Basico/src/com/Conceptos/Arrays.java
@@ -0,0 +1,10 @@
+package com.Conceptos;
+
+public class Arrays {
+    /*
+    ¡Un ARRAYS es un conjunto de Datos del mismo TIpo!
+     */
+
+    int [] array = new int[3];
+    int array1[] = {0, 1, 2,};
+}
